2022e8bdce168d12739e77e35dd73e0e1c0db5b8
ALex Zhao <alex.zhao.superman@gmail.com>
2023-01-15 16:06:57 -0800

    adjusted cutoff a bit

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from battles import *
 from strategy import *
 
 
-army1 = Army({'warrior': 75, 'archer': 75, 'rider': 0, 'defender': 0, 'mindbender': 0, 'knight': 58, 'catapult': 55, 'swordsman': 91, 'cloak': 0, 'giant': 0, 'boat': 32, 'ship': 0, 'battleship': 0})
-army2 = Army({'warrior': 0, 'archer': 0, 'rider': 0, 'defender': 0, 'mindbender': 170, 'knight': 0, 'catapult': 0, 'swordsman': 0, 'cloak': 0, 'giant': 25, 'boat': 0, 'ship': 0, 'battleship': 0})
+army1 = Army({'warrior': 36, 'archer': 0, 'rider': 68, 'defender': 53, 'mindbender': 0, 'knight': 52, 'catapult': 0, 'swordsman': 0, 'cloak': 0, 'giant': 0, 'boat': 70, 'ship': 0, 'battleship': 0})
+army2 = Army({'warrior': 0, 'archer': 0, 'rider': 0, 'defender': 0, 'mindbender': 0, 'knight': 0, 'catapult': 0, 'swordsman': 0, 'cloak': 0, 'giant': 0, 'boat': 0, 'ship': 0, 'battleship': 38})
 
-battle = Battle(army1,army2,1416,"battle",5)
+battle = Battle(army1,army2,854,"battle",5)
 print(army1.cost())
 print(army2.cost())
-print(battle.battlelogEV(1800))
+print(battle.battlelogEV(779))
 # strategy = Strategy.battleSolver(Battle(army1,Army(),800,"name",5),3000)
 # print(
 #     strategy.battle.attacker.armydict,
